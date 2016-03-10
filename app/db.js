16436048e796d95fed4cf4ae12d1775880017541
Think.Web <e.faridhidayat@gmail.com>
2016-03-10 13:55:28 +0700

    db provider

--- app/db.js
+++ app/db.js
@@ -3,18 +3,77 @@
 'use strict';
 
 angular.module('App')
-.constant('config.db',
-  {
-    name:     'app.db',
-    version:  1,
-    scheme:   '0.0.1',
-    logs:    [{1: '0.0.1'}],
-    schemes: {
-      '0.0.1': [
-        'CREATE TABLE IF NOT EXISTS table_name       (id INTEGER PRIMARY KEY, column_name TEXT, column_name INTEGER)'
-      ]
-    },
-    collections: {}
-  });
+.constant('configDb',
+{
+  name:     'app.db',
+  version:  1,
+  scheme:   '0.0.1',
+  logs:    [{1: '0.0.1'}],
+  schemes: {
+    '0.0.1': [
+      'CREATE TABLE IF NOT EXISTS locations       (id INTEGER PRIMARY KEY, column_name TEXT, column_name INTEGER)'
+    ]
+  },
+  collections: {}
+})
+
+.provider('appDb', function(configDb){
+
+  this.db = null;
+  this.openDb = function() {
+    this.db = window.sqlitePlugin.openDatabase({name: configDb.name, location: 1});
+  };
+
+  this.init = function(callback) {
+
+    if(localStorage.getItem('db.built')) {
+      if(callback) callback(null, {message: 'The database is built.', status: 0});
+      return;
+    }
+
+    var
+    that = this,
+    db = configDb,
+    scheme = db.scheme,
+    schemes = db.schemes[scheme];
+    that.db.transaction(function(tx) {
+      // execute db from user configuration
+      for (var j = 0; j < schemes.length; j++) {
+        tx.executeSql(schemes[j]);
+      }
+      if(callback) callback({message: 'Initialization database successful', status: 1});
+      localStorage.setItem('db.built', true);
+    });
+  };
+
+  this.query = function(sql, param, cb) {
+    this.db.transaction(function(tx) {
+      tx.executeSql(sql, param, function(tx, res) {
+        if(cb) cb(res);
+      }, function(e) {
+        if(cb) cb(null, e);
+      });
+    });
+  };
+
+  this.batch = function(sql, params, cb) {
+    this.db.transaction(function(tx) {
+      for (var i in params) {
+        tx.executeSql(sql, params[i], function(tx, res) {
+          // if(cb) cb(res);
+        }, function(e) {
+          if(cb) cb(null, e);
+        });
+      }
+      if(cb) cb(res);
+    });
+  };
+
+  this.$get = function() {
+    return this;
+  };
+
+});
+
 
 })();
